702e70ee75da6758e1c814650f014f1e1ee0cbbd
Emanuela Boros <emanuela.boros@gmail.com>
2021-05-11 15:18:26 +0200

    removing useless commets

--- embeddings/__init__.py
+++ embeddings/__init__.py
@@ -7,6 +7,3 @@ __all__ = [
 ]
 from .camembert_embedding import CamembertEmbedding
 from .bert_embedding import BertEmbedding
-#from .flair_embedding import FlairEmbedding	#Adrián
-#from .roberta_embedding import RobertaEmbedding	#Adrián
-#from .xlm_roberta_embedding import XLMRobertaEmbedding	#Adrián
